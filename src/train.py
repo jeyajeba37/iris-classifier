a54e675031f81a6af21efb4ba96586fa42300b13
Jeyaprakash Jebapandian <jeyajeba37@gmail.com>
2025-11-30 16:46:28 +0000

    Initial commit: Iris decision tree project

---
+++ src/train.py
@@ -0,0 +1,35 @@
+from sklearn.datasets import load_iris
+iris = load_iris()
+X = iris.data      # shape (150, 4)
+y = iris.target    # shape (150,)
+print(iris.feature_names, iris.target_names)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(random_state=42)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("True labels:", y_test[:5])
+print("DecisionTreeClassifier Predictions:", y_pred[:5])
+
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+print("DecisionTreeClassifier Accuracy:", accuracy)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:\n", cm)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+model2 = KNeighborsClassifier(n_neighbors=5)
+model2.fit(X_train, y_train)
+y_pred2 = model2.predict(X_test)
+print("\n K NeighbourClassifier Predictions:", y_pred2[:5])
+print("k-NN accuracy:", accuracy_score(y_test, y_pred2))
